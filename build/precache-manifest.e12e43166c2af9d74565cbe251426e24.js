aab76ad1209739804d4c003ae2b9aaf3179504c9
Ahsan Masood <ahsan21916@gmail.com>
2019-03-27 16:31:08 +0500

    misc

---
+++ build/precache-manifest.e12e43166c2af9d74565cbe251426e24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demo-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "db7ffb4ffc3f496ab832",
+    "url": "/demo-app/static/js/runtime~main.8553000d.js"
+  },
+  {
+    "revision": "6f5629c2af292f74d4a7",
+    "url": "/demo-app/static/js/main.5801bf48.chunk.js"
+  },
+  {
+    "revision": "0571b69cddc311e66ec0",
+    "url": "/demo-app/static/js/2.b934461c.chunk.js"
+  },
+  {
+    "revision": "6f5629c2af292f74d4a7",
+    "url": "/demo-app/static/css/main.e4111d45.chunk.css"
+  },
+  {
+    "revision": "7cdd6f0418ab33c5f2b71228ff4cb548",
+    "url": "/demo-app/index.html"
+  }
+];
